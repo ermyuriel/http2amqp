5448f59184ead90c9b375c213c7b1591f4d97a76
ermyuriel@gmail.com <ermyuriel@gmail.com>
2019-03-22 12:32:54 -0600

    fix dependency build

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -xe
-go mod download
+go get -u -v "github.com/eucj/amqphelper"
+go get -u -v "github.com/eucj/gostructs"
 GOOS=linux GOARCH=amd64 go build -o bin/application -ldflags="-s -w"
