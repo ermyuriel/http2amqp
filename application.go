041791f62eb78888c09b4cee4028f753f48ed38a
ermyuriel@gmail.com <ermyuriel@gmail.com>
2019-03-22 12:39:30 -0600

    fix

---
+++ application.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/eucj/amqphelper"
+)
+
+func main() {
+	exchanges := strings.Split(os.Getenv("AMQP_EXCHANGES"), ",")
+	paths := strings.Split(os.Getenv("RECEIVE_PATHS"), ",")
+	routingKeys := strings.Split(os.Getenv("AMQP_ROUTING_KEYS"), ",")
+
+	for i := range paths {
+		conf := amqphelper.Configuration{Host: os.Getenv("AMQP_HOST"), ContentType: "application/json", RoutingKey: routingKeys[i], Exchange: exchanges[i]}
+		q, err := amqphelper.GetQueue(&conf)
+		if err != nil {
+			log.Panicln(err)
+		}
+		sq := serverQueue{Queue: q, Path: paths[i]}
+		http.HandleFunc(sq.Path, sq.serve)
+	}
+
+	http.HandleFunc(os.Getenv("HEALTHCHECK_PATH"), healthcheck)
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("RECEIVE_PORT"), nil))
+}
+
+func healthcheck(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("{\"Health\":\"OK\"}"))
+}
