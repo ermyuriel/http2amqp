2af246f7d03ce4db4a859a4f5b2bdd362f1acbdb
ermyuriel@gmail.com <ermyuriel@gmail.com>
2019-03-22 12:18:49 -0600

    update build.sh to verbose go get

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -xe
-go get -u -v "github.com/eucj/http2amqp"
+go get -u -v "github.com/eucj/amqphelper"
 GOOS=linux GOARCH=amd64 go build -o bin/application -ldflags="-s -w"
